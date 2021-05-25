227b805131ab0a136f1532c33005d5eadf6d38c4
Dlo Bagari <dlo.bagari@forcepoint.com>
2021-05-25 10:49:02 +0100

    update installation script

--- fp-csg-export-azure-sentinel-tr/fp-csg-export-azure-sentinel-installer.sh
+++ fp-csg-export-azure-sentinel-tr/fp-csg-export-azure-sentinel-installer.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-if [ ! -x /usr/bin/wget ] ; then
-    command -v wget >/dev/null 2>&1 || { echo >&2 "Please install wget or set it in your path. Aborting."; exit 1; }
-fi
-wget https://dl.google.com/go/go1.15.1.linux-amd64.tar.gz
-sudo tar -zxvf go1.15.1.linux-amd64.tar.gz -C /usr/local
-echo "export GOROOT=/usr/local/go" | sudo tee -a /etc/profile
-echo "export PATH=$PATH:/usr/local/go/bin" | sudo tee -a /etc/profile
-source /etc/profile
 chmod +x fp-csg-sentinel
 mkdir /var/forpcepoint-csg
 mv fp-csg-sentinel.service /etc/systemd/system/
